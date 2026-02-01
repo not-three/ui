3a09cbc634d02da33ccead1a5e57501030ce92d0
scolastico <scolastico@users.noreply.github.com>
2026-02-01 10:08:00 +0000

    Deploy preview for PR 92 🛫

---
+++ pr-preview/pr-92/_nuxt/Bh_RwtbC.js
@@ -0,0 +1,1 @@
+import{_ as t}from"./ChP7LFOZ.js";import{_ as o,c as n,o as _}from"./CR7HZyAq.js";const c={};function s(r,a){const e=t;return _(),n(e,{"open-settings":!0})}const m=o(c,[["render",s]]);export{m as default};
---
+++ pr-preview/pr-92/_nuxt/D9LSEOXc.js
@@ -0,0 +1,1 @@
+import{_ as e}from"./ChP7LFOZ.js";import{_,c,o as n}from"./CR7HZyAq.js";const t={};function r(s,a){const o=e;return n(),c(o)}const m=_(t,[["render",r]]);export{m as default};
