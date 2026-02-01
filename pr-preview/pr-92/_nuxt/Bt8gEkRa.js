eacb916e6a79174ddccc32e9c20018b8428dc9b7
scolastico <scolastico@users.noreply.github.com>
2026-02-01 10:21:40 +0000

    Remove preview for PR 92 🛬

--- pr-preview/pr-92/_nuxt/Bt8gEkRa.js
+++
@@ -1,1 +0,0 @@
-import{_ as n}from"./ChP7LFOZ.js";import{d as a,u as t,o as s,c as _,a as p}from"./CR7HZyAq.js";const f=a({__name:"[id]",setup(c){const e=t().params.id;return(r,m)=>{const o=n;return s(),_(o,{"open-file":p(e)},null,8,["open-file"])}}});export{f as default};
