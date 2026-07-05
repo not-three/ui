a141f2b03aba3b5101b74ec9381975b94420cb67
scolastico <scolastico@users.noreply.github.com>
2026-07-05 02:52:50 +0000

    Deploy preview for PR 112 🛫

---
+++ pr-preview/pr-112/_nuxt/BFJp_LKQ.js
@@ -0,0 +1,1 @@
+import{_ as n}from"./fUFKDw5s.js";import{d as a,u as t,o as s,c as _,a as p}from"./BxXzI7lo.js";const f=a({__name:"[id]",setup(c){const e=t().params.id;return(r,m)=>{const o=n;return s(),_(o,{"open-file":p(e)},null,8,["open-file"])}}});export{f as default};
